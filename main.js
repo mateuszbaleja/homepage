0a3ebca53f6e33d2b7268605a8751aa59128d77a
Mateusz Baleja <mateusz.baleja@gmail.com>
2020-05-23 08:44:59 +0200

    add menu button

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ const age = 29;
 
 
 console.log(`Hej, jestem ${firstName} i już niedługo będę mógł ${age} zrobić dla Ciebie ekstra stronę`)
+
+
+const empty = document.querySelector('.article');
+
+console.log(empty);
+
+
+function welcome(firstName, age){
+    console.log(`Cześć użytkowniku! Mam na imię ${firstName} i mam ${age} lat`);
+
+}
+
+welcome('Mateusz', 29)
+
+
+function greet(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+
+}
+
+greet('.empty-js', 'Witaj na mojej stronie tworzonej na potrzeby WTF');
+
+
+const menu = document.querySelector('.menu--js')
+menu.addEventListener('click', ()=>{
+    const nav = document.querySelector('.transfer--js');
+    nav.classList.toggle('transfer--open');
+})
